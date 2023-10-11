af7567e297a72b7edef86e150abde9f06eee69a6
vox <vox@vox.me>
2023-10-11 08:41:41 +0800

    add view

--- resource.h
+++ resource.h
@@ -17,18 +17,19 @@
 #define IDC_LABEL                       3006
 #define IDC_APPICON                     3007
 #define IDC_HOURGLASS                   3008
-
 #define ID_NAV_BACK                     4000
 #define ID_NVA_FOR                      4001
 #define ID_EDIT_MODIFY                  4002
 #define ID_APP_SETTING                  4003
+#define ID_VIEW_GOBACK                  32774
+#define ID_VIEW_GONEXT                  32775
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        201
-#define _APS_NEXT_COMMAND_VALUE         32774
+#define _APS_NEXT_COMMAND_VALUE         32776
 #define _APS_NEXT_CONTROL_VALUE         1000
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
